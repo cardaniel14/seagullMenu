2e2df5446bf548884da34688143d8fd1a2b407f1
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-08-24 16:56:09 +0100

    adicionando recetas al DOM

--- script.js
+++ script.js
@@ -3,25 +3,50 @@ let jsonData; // Variable para almacenar el JSON cargado
 fetch('platos.json')
   .then(response => response.json())
   .then(data => {
-    jsonData = data; 
+    breakfast = data; 
     prueba()
 
   })
   
-  function prueba(){    
-    for(let plato of jsonData){
-        let div = document.querySelector(".platos");
-        let h2 = document.createElement("h2");
-        let img = document.createElement("img");
-        img.src = plato.img;
-        h2.textContent=plato.name;
-        div.appendChild(h2)
-        div.appendChild(img)
+  // function prueba(){    
+  //   for(let plato of breakfast){
+  //     console.log(breakfast)
+  //       // let div = document.querySelector(".platos");
+  //       let h2 = document.querySelector(".plateName");
+  //       let img = document.querySelector(".photoPlate");
+  //       let price = document.querySelector(".price");
+  //       let ingredients = document.querySelector(".ingredients");
+  //       img.src = plato.img;
+  //       h2.textContent=plato.name;
+  //       price.textContent=plato.price;
+  //       ingredients.textContent=plato.ingredients;
         
-    }
+  //   }
     
     
-  }
+  // }
+function prueba(){
+  breakfast.forEach(e => {
+    let div = document.querySelector(".platos");
+    let h2 = document.createElement("h2");
+    let photo = document.createElement("img");
+    let ingredients = document.createElement("p");
+    let price = document.createElement("p");
+    price.className="price";
+    price.textContent=`${e.price}€`;
+    ingredients.textContent = e.ingredients
+    ingredients.className="ingredients";
+    h2.textContent=e.name;
+    h2.className="plateName"
+    photo.className="photoPlate"
+    photo.src=e.img;
+    div.appendChild(photo)
+    div.appendChild(h2)
+    div.appendChild(ingredients)
+    div.appendChild(price);
+    
+  });
+}
 
 let loader = document.querySelector(".loader");
 window.addEventListener("load", ()=>{
