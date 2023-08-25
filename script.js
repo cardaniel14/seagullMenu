a64ea7c61949287e00af641f7cf6495de5270583
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-08-25 22:17:09 +0100

    modificacion

--- script.js
+++ script.js
@@ -1,35 +1,97 @@
 let loader = document.querySelector(".loader");
-window.addEventListener("load", ()=>{
-    loader.classList.toggle("loader2")
+window.addEventListener("load", () => {
+  loader.classList.toggle("loader2")
 });
 
-let jsonData; // Variable para almacenar el JSON cargado
+let dataJson; // Variable para almacenar el JSON cargado
 
-fetch('breakfast.json')
+fetch('menu.json')
   .then(response => response.json())
   .then(data => {
-    breakfast = data; 
-    sectionBreakFast()
+    dataJson = data;
+    if (window.location.pathname === "/lunch.html") {
+      sectionLunch()
+    } else if (window.location.pathname === "/breakfast.html") {
+      sectionBreakFast();
+    } else if (window.location.pathname === "/desserts.html") {
+      sectionDesserts();
+    }
 
-  })    
-    
-function sectionBreakFast(){
-  const cardSelector = document.getElementById("card-selector");
-      breakfast.forEach(result => {
-        console.log(result);
+    sectionBreakFast();
+    sectionLunch();
+    sectionDesserts();
+  })
+
+  function sectionBreakFast() {
+    const cardSelector = document.getElementById("card-selector");
+    dataJson[0].forEach(result => {
+      let arr = result.breakfast
+      arr.forEach(result => {
         let card = `
-                      <img class="photoPlate mx-auto d-block" src="${result.img}">
-                      <h2 class="plateName">${result.name}</h2>
-                      <p class="ingredients text-center">${result.ingredients}</p>
-                      <p class="price text-center">${result.price}€</p>
-                    `;
-
+                        <img class="photoPlate mx-auto d-block" src="${result.img}">
+                        <h2 class="plateName">${result.name}</h2>
+                        <p class="ingredients text-center">${result.ingredients}</p>
+                        <p class="price text-center">${result.price}€</p>
+                        
+                      `;
+  
         let containerDiv = document.createElement("div"); //create the col div
         containerDiv.setAttribute("class", "col-sm-6 col-md-4 col-lg-3"); //Add the bootstrap col class as needed
         containerDiv.innerHTML = card;
         cardSelector.appendChild(containerDiv);
-      });
+      })
+    });
+  }  
+
+function sectionLunch() {
+  const cardSelector = document.getElementById("card-lunch");
+  // console.log(dataJson[1])
+  dataJson[1].forEach(result => {
+    let arr = result.lunch
+    // console.log(arr)
+    arr.forEach(result => {
+      let card = `
+                        <img class="photoPlate mx-auto d-block" src="${result.img}">
+                        <h2 class="plateName">${result.name}</h2>
+                        <p class="ingredients text-center">${result.ingredients}</p>
+                        <p class="price text-center">${result.price}€</p>
+                        
+                      `;
+
+      let containerDiv = document.createElement("div"); //create the col div
+      containerDiv.setAttribute("class", "col-sm-6 col-md-4 col-lg-3"); //Add the bootstrap col class as needed
+      containerDiv.innerHTML = card;
+      cardSelector.appendChild(containerDiv);
+    })
+  });
 }
+
+function sectionDesserts() {
+  const cardSelector = document.getElementById("card-desserts");
+  // console.log(dataJson[1])
+  dataJson[2].forEach(result => {
+    let arr = result.desserts
+    // console.log(arr)
+    arr.forEach(result => {
+      let card = `
+                        <img class="photoPlate mx-auto d-block" src="${result.img}">
+                        <h2 class="plateName">${result.name}</h2>
+                        <p class="ingredients text-center">${result.ingredients}</p>
+                        <p class="price text-center">${result.price}€</p>
+                        
+                      `;
+
+      let containerDiv = document.createElement("div"); //create the col div
+      containerDiv.setAttribute("class", "col-sm-6 col-md-4 col-lg-3"); //Add the bootstrap col class as needed
+      containerDiv.innerHTML = card;
+      cardSelector.appendChild(containerDiv);
+    })
+  });
+}
+
+
+
+
 
 
 
