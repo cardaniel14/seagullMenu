074bbfbe2a8c36e7b91d5f062858f3d1f82ac574
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-08-25 01:54:31 +0100

    seccion breakfast terminada

--- script.js
+++ script.js
@@ -6,48 +6,53 @@ fetch('platos.json')
     breakfast = data; 
     prueba()
 
-  })
-  
-  // function prueba(){    
-  //   for(let plato of breakfast){
-  //     console.log(breakfast)
-  //       // let div = document.querySelector(".platos");
-  //       let h2 = document.querySelector(".plateName");
-  //       let img = document.querySelector(".photoPlate");
-  //       let price = document.querySelector(".price");
-  //       let ingredients = document.querySelector(".ingredients");
-  //       img.src = plato.img;
-  //       h2.textContent=plato.name;
-  //       price.textContent=plato.price;
-  //       ingredients.textContent=plato.ingredients;
-        
-  //   }
+  })    
     
+// function prueba(){
+//   breakfast.forEach(e => {
+//     let div = document.querySelector(".platos");
+//     let h2 = document.createElement("h2");
+//     let photo = document.createElement("img");
+//     let ingredients = document.createElement("p");
+//     let price = document.createElement("p");
+//     price.className="price";
+//     price.textContent=`${e.price}€`;
+//     ingredients.textContent = e.ingredients;
+//     ingredients.className="ingredients";
     
-  // }
+//     h2.textContent=e.name;
+//     h2.className="plateName"
+//     photo.className="photoPlate"
+//     photo.src=e.img;
+//     div.appendChild(photo)
+//     div.appendChild(h2)
+//     div.appendChild(ingredients)
+//     div.appendChild(price);
+
+    
+//   });
+// }
+
 function prueba(){
-  breakfast.forEach(e => {
-    let div = document.querySelector(".platos");
-    let h2 = document.createElement("h2");
-    let photo = document.createElement("img");
-    let ingredients = document.createElement("p");
-    let price = document.createElement("p");
-    price.className="price";
-    price.textContent=`${e.price}€`;
-    ingredients.textContent = e.ingredients
-    ingredients.className="ingredients";
-    h2.textContent=e.name;
-    h2.className="plateName"
-    photo.className="photoPlate"
-    photo.src=e.img;
-    div.appendChild(photo)
-    div.appendChild(h2)
-    div.appendChild(ingredients)
-    div.appendChild(price);
-    
-  });
+  const cardSelector = document.getElementById("card-selector");
+      breakfast.forEach(result => {
+        console.log(result);
+        let card = `
+                      <img class="photoPlate mx-auto d-block" src="${result.img}">
+                      <h2 class="plateName">${result.name}</h2>
+                      <p class="ingredients text-center">${result.ingredients}</p>
+                      <p class="price text-center">${result.price}€</p>
+                    `;
+
+        let containerDiv = document.createElement("div"); //create the col div
+        containerDiv.setAttribute("class", "col-sm-6 col-md-4 col-lg-3"); //Add the bootstrap col class as needed
+        containerDiv.innerHTML = card;
+        cardSelector.appendChild(containerDiv);
+      });
 }
 
+                  // <div class="card mb-3">
+                  //   <div class="card-body">
 let loader = document.querySelector(".loader");
 window.addEventListener("load", ()=>{
     loader.classList.toggle("loader2")
